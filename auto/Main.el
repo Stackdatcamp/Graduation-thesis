cdb4e47bf13899685d1a13cc1e62224b85c314f7
Stackdatcamp <stackdatcamp@gmail>
2017-05-06 00:27:30 +0700

    Cập nhập phần định nghĩa mô hình SVM

--- auto/Main.el
+++ auto/Main.el
@@ -5,29 +5,22 @@
                      '(("report" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("scrextend" "fontsize=13pt") ("vietnam" "utf8") ("geometry" "a4paper") ("placeins" "section") ("caption" "justification=centering") ("hyperref" "colorlinks=true" "citecolor=blue" "linkcolor=blue") ("hypcap" "all") ("biblatex" "backend=bibtex" "bibstyle=luanan" "sorting=ydnt" "style=authoryear")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
-    "./Cover/front-cover"
     "./Ch1/credit-risk"
     "report"
     "rep10"
+    "graphicx"
+    "color"
+    "framed"
+    "amsmath"
+    "alltt"
     "scrextend"
     "vietnam"
     "verbatim"
     "listings"
     "afterpage"
     "xcolor"
-    "graphicx"
     "enumitem"
     "indentfirst"
     "mathptmx"
@@ -40,11 +33,32 @@
     "caption"
     "hyperref"
     "hypcap"
-    "biblatex")
+    "biblatex"
+    "upquote")
    (TeX-add-symbols
+    '("hlkwd" 1)
+    '("hlkwc" 1)
+    '("hlkwb" 1)
+    '("hlkwa" 1)
+    '("hlstd" 1)
+    '("hlopt" 1)
+    '("hlcom" 1)
+    '("hlstr" 1)
+    '("hlnum" 1)
+    "maxwidth"
+    "hlipl"
+    "FrameCommand"
     "Title"
     "Author")
+   (LaTeX-add-environments
+    "kframe"
+    "knitrout")
    (LaTeX-add-bibliographies
-    "reference"))
- :latex)
+    "reference")
+   (LaTeX-add-xcolor-definecolors
+    "fgcolor"
+    "shadecolor"
+    "messagecolor"
+    "warningcolor"
+    "errorcolor")))
 
